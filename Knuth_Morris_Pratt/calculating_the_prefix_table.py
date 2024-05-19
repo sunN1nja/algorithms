8697d9fd72449cf8d7bf164a9fbc42c0d276a3b8
sunN1nja <sunn1nja@yandex.ru>
2024-05-20 00:44:10 +0300

    Create calculating_the_prefix_table.py

---
+++ Knuth_Morris_Pratt/calculating_the_prefix_table.py
@@ -0,0 +1,17 @@
+def computeLPSArray(pattern):
+    m = len(pattern)
+    lps = [0] * m
+    length = 0
+    i = 1
+    while i < m:
+        if pattern[i] == pattern[length]:
+            length += 1
+            lps[i] = length
+            i += 1
+        else:
+            if length != 0:
+                length = lps[length-1]
+            else:
+                lps[i] = 0
+                i += 1
+    return lps
