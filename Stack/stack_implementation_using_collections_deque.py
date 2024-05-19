d4481e70cc9cedfff0a5865272ddfbe56e1e2b55
sunN1nja <sunn1nja@yandex.ru>
2024-05-19 23:07:25 +0300

    Create stack_implementation_using_collections_deque.py

---
+++ Stack/stack_implementation_using_collections_deque.py
@@ -0,0 +1,36 @@
+from collections import deque
+
+class Stack:
+    def __init__(self):
+        self.items = deque()
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        if not self.is_empty():
+            return self.items.pop()
+        else:
+            raise IndexError("pop from empty stack")
+
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+        else:
+            raise IndexError("peek from empty stack")
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def size(self):
+        return len(self.items)
+
+# Пример использования
+stack = Stack()
+stack.push(1)
+stack.push(2)
+stack.push(3)
+print(stack.pop())  # Вывод: 3
+print(stack.peek())  # Вывод: 2
+print(stack.size())  # Вывод: 2
+print(stack.is_empty())  # Вывод: False
